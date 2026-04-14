90797647253374838a0857ab2408fa411e9b612a
AyanSingal <ayansingal676@gmail.com>
2026-04-14 18:37:47 -0400

    Started setting up glTF model loading

--- VulkanModel.cpp
+++ VulkanModel.cpp
@@ -1,3 +1,7 @@
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#include <tiny_gltf.h>
+
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "VulkanModel.h"
 #include <tiny_obj_loader.h>
@@ -13,44 +17,183 @@ void VulkanModel::init(VulkanContext &context, ResourceManager &resourceManager,
 
 void VulkanModel::load(const std::string &modelPath)
 {
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
-    std::string warn, err;
+    tinygltf::Model gltfModel;
+    tinygltf::TinyGLTF loader;
+    std::string err, warn;
 
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, modelPath.c_str()))
+    bool ret = loader.LoadASCIIFromFile(&gltfModel, &err, &warn, modelPath);
+
+    if (!warn.empty())
     {
-        throw std::runtime_error(err);
+        std::cout << "glTF warning: " << warn << std::endl;
+    }
+    if (!err.empty())
+    {
+        std::cout << "glTF warning: " << err << std::endl;
+    }
+    if (!ret)
+    {
+        throw std::runtime_error("Failed to load glTF: " + modelPath);
     }
 
-    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
-
-    for (const auto &shape : shapes)
+    for (const auto &mesh : gltfModel.meshes)
     {
-        for (const auto &index : shape.mesh.indices)
+        for (const auto &primitive : mesh.primitives)
         {
-            Vertex vertex{};
+            std::vector<Vertex> vertices;
+            std::vector<uint32_t> indices;
 
-            vertex.pos = {
-                attrib.vertices[3 * index.vertex_index + 0],
-                attrib.vertices[3 * index.vertex_index + 1],
-                attrib.vertices[3 * index.vertex_index + 2]};
+            // Extract vertex positions
+            const tinygltf::Accessor &posAccessor = gltfModel.accessors[primitive.attributes.find("POSITION")->second];
+            const tinygltf::BufferView &posView = gltfModel.bufferViews[posAccessor.bufferView];
+            const tinygltf::Buffer &posBuffer = gltfModel.buffers[posView.buffer];
+            const float *positions = reinterpret_cast<const float *>(&posBuffer.data[posView.byteOffset + posAccessor.byteOffset]);
 
-            vertex.texCoord = {
-                attrib.texcoords[2 * index.texcoord_index + 0],
-                1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
-
-            vertex.color = {1.0f, 1.0f, 1.0f};
-
-            if (uniqueVertices.count(vertex) == 0)
+            // Extract texture coordinates
+            const float *texcoords = nullptr;
+            if (primitive.attributes.find("TEXCOORD_0") != primitive.attributes.end())
             {
-                uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                const tinygltf::Accessor &texAccessor = gltfModel.accessors[primitive.attributes.find("TEXCOORD_0")->second];
+                const tinygltf::BufferView &texView = gltfModel.bufferViews[texAccessor.bufferView];
+                const tinygltf::Buffer &texBuffer = gltfModel.buffers[texView.buffer];
+                texcoords = reinterpret_cast<const float *>(&texBuffer.data[texView.byteOffset + texAccessor.byteOffset]);
+            }
+
+            // Populate the vertices array for this primitive
+            for (size_t i = 0; i < posAccessor.count; i++)
+            {
+                Vertex vertex{};
+                vertex.pos = {
+                    positions[i * 3 + 0],
+                    positions[i * 3 + 1],
+                    positions[i * 3 + 2]};
+                vertex.color = {1.0f, 1.0f, 1.0f};
+                if (texcoords)
+                {
+                    vertex.texCoord = {
+                        texcoords[i * 2 + 0],
+                        texcoords[i * 2 + 1]};
+                }
+                else
+                {
+                    vertex.texCoord = {0.0f, 0.0f};
+                }
                 vertices.push_back(vertex);
             }
 
-            indices.push_back(uniqueVertices[vertex]);
+            // Extract indices
+            const tinygltf::Accessor &indexAccessor = gltfModel.accessors[primitive.indices];
+            const tinygltf::BufferView &indexView = gltfModel.bufferViews[indexAccessor.bufferView];
+            const tinygltf::Buffer &indexBuffer = gltfModel.buffers[indexView.buffer];
+            const unsigned char *indexData = &indexBuffer.data[indexView.byteOffset + indexAccessor.byteOffset];
+
+            // Populate indices array for this primitive
+            for (size_t i = 0; i < indexAccessor.count; i++)
+            {
+                uint32_t index;
+                if (indexAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+                {
+                    index = reinterpret_cast<const uint16_t *>(indexData)[i];
+                }
+                else if (indexAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+                {
+                    index = reinterpret_cast<const uint32_t *>(indexData)[i];
+                }
+                else if (indexAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+                {
+                    index = indexData[i];
+                }
+                else
+                {
+                    throw std::runtime_error("Unsupported index type");
+                }
+                indices.push_back(index);
+            }
+
+            // Start setting up the submesh
+            SubMesh subMesh{};
+            subMesh.indexCount = static_cast<uint32_t>(indices.size());
+            subMesh.materialIndex = primitive.material;
+
+            // Fill in the vertex data for the submesh
+            VkDeviceSize vertexSize = sizeof(Vertex) * vertices.size();
+            VkBuffer vertexStaging;
+            VkDeviceMemory vertexStagingMem;
+            resourceManager->createBuffer(vertexSize,
+                                          VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                                          vertexStaging, vertexStagingMem);
+            void *data;
+            vkMapMemory(context->device, vertexStagingMem, 0, vertexSize, 0, &data);
+            memcpy(data, vertices.data(), vertexSize);
+            vkUnmapMemory(context->device, vertexStagingMem);
+            resourceManager->createBuffer(vertexSize,
+                                          VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                                          subMesh.vertexBuffer, subMesh.vertexBufferMemory);
+            resourceManager->copyBuffer(vertexStaging, subMesh.vertexBuffer, vertexSize);
+            vkDestroyBuffer(context->device, vertexStaging, nullptr);
+            vkFreeMemory(context->device, vertexStagingMem, nullptr);
+
+            // Fill in the index data for the submesh
+            VkDeviceSize indexSize = sizeof(uint32_t) * indices.size();
+            VkBuffer indexStaging;
+            VkDeviceMemory indexStagingMem;
+            resourceManager->createBuffer(indexSize,
+                                          VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                                          indexStaging, indexStagingMem);
+            void *data;
+            vkMapMemory(context->device, indexStagingMem, 0, indexSize, 0, &data);
+            memcpy(data, indices.data(), indexSize);
+            vkUnmapMemory(context->device, indexStagingMem);
+            resourceManager->createBuffer(indexSize,
+                                          VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                                          subMesh.indexBuffer, subMesh.indexBufferMemory);
+            resourceManager->copyBuffer(indexStaging, subMesh.indexBuffer, indexSize);
+            vkDestroyBuffer(context->device, indexStaging, nullptr);
+            vkFreeMemory(context->device, indexStagingMem, nullptr);
+            subMeshes.push_back(subMesh);
         }
     }
+
+    std::cout << "Loaded " << subMeshes.size() << " sub-meshes" << std::endl;
+
+    // tinyobj::attrib_t attrib;
+    // std::vector<tinyobj::shape_t> shapes;
+    // std::vector<tinyobj::material_t> materials;
+    // std::string warn, err;
+
+    // if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, modelPath.c_str()))
+    // {
+    //     throw std::runtime_error(err);
+    // }
+
+    // std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+    // for (const auto &shape : shapes)
+    // {
+    //     for (const auto &index : shape.mesh.indices)
+    //     {
+    //         Vertex vertex{};
+
+    //         vertex.pos = {
+    //             attrib.vertices[3 * index.vertex_index + 0],
+    //             attrib.vertices[3 * index.vertex_index + 1],
+    //             attrib.vertices[3 * index.vertex_index + 2]};
+
+    //         vertex.texCoord = {
+    //             attrib.texcoords[2 * index.texcoord_index + 0],
+    //             1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
+
+    //         vertex.color = {1.0f, 1.0f, 1.0f};
+
+    //         if (uniqueVertices.count(vertex) == 0)
+    //         {
+    //             uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+    //             vertices.push_back(vertex);
+    //         }
+
+    //         indices.push_back(uniqueVertices[vertex]);
+    //     }
+    // }
 }
 
 void VulkanModel::createVertexBuffer()
@@ -91,11 +234,6 @@ void VulkanModel::createIndexBuffer()
     resourceManager->copyBuffer(stagingBuffer, indexBuffer, bufferSize);
     vkDestroyBuffer(context->device, stagingBuffer, nullptr);
     vkFreeMemory(context->device, stagingBufferMemory, nullptr);
-}
-
-size_t VulkanModel::getIndicesSize()
-{
-    return indices.size();
 }
 
 void VulkanModel::cleanup()
--- VulkanModel.h
+++ VulkanModel.h
@@ -4,14 +4,20 @@
 #include "ResourceManager.h"
 #include "Vertex.h"
 
+struct SubMesh{
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+    uint32_t indexCount;
+    int materialIndex;
+};
+
 class VulkanModel
 {
 public:
-    VkBuffer vertexBuffer;
-    VkBuffer indexBuffer;
-
+    std::vector<SubMesh> subMeshes;
     void init(VulkanContext &context, ResourceManager &resourceManager, const std::string &modelPath);
-    size_t getIndicesSize();
 
     void cleanup();
     
@@ -20,8 +26,6 @@ private:
     ResourceManager *resourceManager = nullptr;
     std::vector<Vertex> vertices;
     std::vector<uint32_t> indices;    
-    VkDeviceMemory vertexBufferMemory;
-    VkDeviceMemory indexBufferMemory;
     std::string modelPath;
 
     void load(const std::string &modelPath);
